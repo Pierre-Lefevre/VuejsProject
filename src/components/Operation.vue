abe0b659130912cd1716ac74ea4f60716c8f0b0b
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2018-01-16 18:51:18 +0100

    Generate and shuffle operations.

--- src/components/Operation.vue
+++ src/components/Operation.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-    {{ $route.params.id }}
+    {{ operations }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'operation'
+  name: 'operation',
+  data () {
+    return {
+      operations: this.generateOperations()
+    }
+  },
+  methods: {
+    generateOperations () {
+      let operations = []
+
+      for (let i = 1; i <= 10; i++) {
+        if ((Math.random() > 0.5) ? 1 : 0) {
+          operations.push(this.$route.params.id + ' * ' + i)
+        } else {
+          operations.push(i + ' * ' + this.$route.params.id)
+        }
+      }
+
+      for (let i = 0; i < operations.length; i++) {
+        let operation = operations.splice(i, 1)[0]
+        operations.splice(Math.floor(Math.random() * operations.length), 0, operation)
+      }
+
+      return operations
+    }
+  }
 }
 </script>
 
