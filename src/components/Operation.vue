cc4a570f6d41e267928864dc5bcb5ed2e961aa3d
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2018-01-17 16:53:41 +0100

    Add data in localstorage, save number of attempts and time.

--- src/components/Operation.vue
+++ src/components/Operation.vue
@@ -21,11 +21,13 @@ export default {
     return {
       table: parseInt(this.$route.params.id),
       index: 0,
-      operations: []
+      operations: [],
+      startTimestamp: 0
     }
   },
   created () {
     this.generateOperations()
+    this.startTimestamp = Date.now()
   },
   methods: {
     generateOperations () {
@@ -43,6 +45,7 @@ export default {
         }
         operation.goodAnswer = operation.facteur1 * operation.facteur2
         operation.answers = JSON.parse(JSON.stringify(answers))
+        operation.nbAttempts = 0
         this.shuffleArray(operation.answers)
         this.operations.push(operation)
       }
@@ -61,12 +64,15 @@ export default {
     },
     validAnswer (indexAnswer) {
       console.log(localStorage)
-      localStorage[indexAnswer] = 'a'
+      this.operations[this.index].nbAttempts++
       if (this.operations[this.index].facteur1 * this.operations[this.index].facteur2 === this.operations[this.index].answers[indexAnswer]) {
+        this.operations[this.index].time = Date.now() - this.startTimestamp
         if (this.index === 9) {
-          this.$router.push({name: 'Home'})
+          localStorage.data = JSON.stringify({operations: this.operations})
+          this.$router.push({name: 'Score'})
         } else {
           this.index++
+          this.startTimestamp = Date.now()
         }
       } else {
         this.operations[this.index].answers.splice(indexAnswer, 1)
@@ -90,7 +96,9 @@ export default {
   .answer-enter-active, .answer-leave-active {
     transition: all 1s;
   }
-  .answer-enter, .answer-leave-to /* .list-leave-active below version 2.1.8 */ {
+
+  .answer-enter, .answer-leave-to /* .list-leave-active below version 2.1.8 */
+  {
     opacity: 0;
     transform: translateY(30px);
   }
