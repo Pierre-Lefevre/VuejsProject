0261fef4f9a425a2de084eb1890ab225538c3d24
solenedorey <solene.dorey@outlook.com>
2018-01-24 12:31:37 +0100

    Stats page in progress

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,56 +1,58 @@
 <template>
   <div>
     <ul>
-      <!--<li :key="i" v-for="(sessionHistory, i) in globalHistory">
-        <ul>
-          <li :key="j" v-for="(operation, j) in sessionHistory">
-            {{ operation.facteur1 }} * {{ operation.facteur2 }}
-          </li>
-        </ul>
-      </li>-->
-      <li :key="i" v-for="(operation, i) in finalTab">
-        <p>{{ operation.facteur1 }} x {{ operation.facteur2 }}</p>
-        <p>Nombre d'erreurs moyen : {{ operation.nbErrors }}</p>
-      </li>
+      <div :key="i" v-for="(factor1, i) in statsTab">
+        <h1>Table du {{ i }}</h1>
+        <p :key="j" v-for="(factor2, j) in factor1">
+          {{ i }} * {{ j }} =>
+          Nombre d'erreurs moyen : {{ factor2.avgErrors }}
+        </p>
+      </div>
     </ul>
-    <pre>{{ globalHistory }}</pre>
-    <p>{{ count }}</p>
-    <p>{{ finalTab.length }}</p>
-    <pre>{{ finalTab }}</pre>
   </div>
 </template>
 
 <script>
+import lsm from '@/services/localStorageManager'
+
 export default {
   name: 'statistics',
   data () {
     return {
       globalHistory: [],
-      finalTab: [],
-      count: 0
+      statsTab: {}
     }
   },
   created () {
-    this.globalHistory = JSON.parse(localStorage.history)
+    this.globalHistory = lsm.getValueUser('history')
+    console.log(this.globalHistory)
     this.updateStats()
   },
   methods: {
     updateStats () {
-      let similar
       this.globalHistory.forEach(function (session) {
         session.forEach(function (operation) {
-          this.count++
-          similar = false
-          for (let i = 0; i < this.finalTab.length; i++) {
-            if (operation.facteur1 === this.finalTab[i].facteur1 && operation.facteur2 === this.finalTab[i].facteur2) {
-              this.finalTab[i].nbErrors = (this.finalTab[i].nbErrors + operation.nbErrors) / 2
-              similar = true
-              break
-            }
+          if (this.statsTab[operation.factor1] === undefined) {
+            this.statsTab[operation.factor1] = {}
           }
-          if (!similar) this.finalTab.push(operation)
+          if (this.statsTab[operation.factor1][operation.factor2] === undefined) {
+            this.statsTab[operation.factor1][operation.factor2] = {nbErrors: 0, count: 0, avgErrors: 0}
+          }
+          if (this.statsTab[operation.factor2] === undefined) {
+            this.statsTab[operation.factor2] = {}
+          }
+          if (this.statsTab[operation.factor2][operation.factor1] === undefined) {
+            this.statsTab[operation.factor2][operation.factor1] = {nbErrors: 0, count: 0, avgErrors: 0}
+          }
+          this.statsTab[operation.factor1][operation.factor2].nbErrors += operation.nbErrors
+          this.statsTab[operation.factor2][operation.factor1].nbErrors += operation.nbErrors
+          this.statsTab[operation.factor1][operation.factor2].count++
+          this.statsTab[operation.factor2][operation.factor1].count++
+          this.statsTab[operation.factor1][operation.factor2].avgErrors = this.statsTab[operation.factor1][operation.factor2].nbErrors / this.statsTab[operation.factor1][operation.factor2].count
+          this.statsTab[operation.factor2][operation.factor1].avgErrors = this.statsTab[operation.factor2][operation.factor1].nbErrors / this.statsTab[operation.factor2][operation.factor1].count
         }.bind(this))
       }.bind(this))
+      console.log(this.statsTab)
     }
   }
 }
@@ -60,12 +62,15 @@ export default {
   ul li ul li:nth-child(odd) {
     background-color: yellowgreen;
   }
+
   ul li ul li:nth-child(even) {
     background-color: hotpink;
   }
+
   pre:nth-child(even) {
     background-color: dodgerblue;
   }
+
   pre:nth-child(odd) {
     background-color: orange;
   }
