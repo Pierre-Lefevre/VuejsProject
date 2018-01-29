f9bfcee6ab02f98dbeeaff0587f03eba1bfeaccd
solenedorey <solene.dorey@outlook.com>
2018-01-29 21:31:06 +0100

    Time succes definition

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,11 @@ for (let i = 1; i <= 10; i++) {
   achievements['table' + i + 'Master'] = {name: 'Maître de la table de ' + i, message: 'Finir la table de ' + i + ' sans faute.', secret: false}
 }
 achievements['tableMaster'] = {name: 'Maître des tables', message: 'Finir toutes les tables sans faute.', secret: false}
+let timeMasterThreshold = 15000
+for (let i = 1; i <= 10; i++) {
+  achievements['table' + i + 'timeMaster'] = {name: 'Table de ' + i + ' en contre-la-montre', message: 'Finir la table de ' + i + ' en moins de ' + timeMasterThreshold / 1000 + ' secondes.', secret: false}
+}
+achievements['timeMaster'] = {name: 'Maître du temps', message: 'Finir toutes les tables en moins de ' + timeMasterThreshold / 1000 + ' secondes.', secret: false}
 achievements['niceTry'] = {name: 'Bien essayé !', message: 'Tenter d\'accéder à une table non référencée.', secret: true}
 
 module.exports = {
@@ -19,6 +24,9 @@ module.exports = {
 
   // Coefficient permettant de déduire le seuil minimal temporel au-dessus duquel nous supposons que l'élève compte sur ses doigts.
   coefficientToDeduceCountingFingersThreshold: 3,
+
+  // Temps en millisecondes définissant les succès liés au temps
+  timeMasterThreshold: timeMasterThreshold,
 
   // Les listes des succès.
   achievements: achievements
