a916f767ebb657da6aae430e11db5fcac3356887
solenedorey <solene.dorey@outlook.com>
2018-01-30 11:45:57 +0100

    Home page navigation updated

--- src/components/HomeAuth.vue
+++ src/components/HomeAuth.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <router-link :to="{name: 'Learn'}" class="btn-primary bg-green" tag="button">Apprentissage</router-link>
-    <router-link :to="{name: 'Operation'}" class="btn-primary bg-red" tag="button" v-if="canAccessTest()">Evaluation</router-link>
+  <div class="wrapper">
+    <router-link :to="{name: 'Learn'}" class="hvr-grow-rotate green-splash" tag="div"><span>Apprentissage</span></router-link>
+    <router-link :to="{name: 'Operation'}" class="hvr-grow-rotate red-splash" tag="div" v-if="canAccessTest()"><span>Evaluation</span></router-link>
   </div>
 </template>
 
@@ -20,5 +20,48 @@ export default {
 </script>
 
 <style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div.green-splash, div.red-splash {
+    width: 30vw;
+    height: 50vh;
+    margin: 0 2rem;
+    cursor: pointer;
+  }
+
+  div.green-splash {
+    background: url("../assets/img/green_splash.svg") no-repeat center center;
+    background-size: contain;
+  }
+
+  div.red-splash {
+    background: url("../assets/img/red_splash.svg") no-repeat center center;
+    background-size: contain;
+  }
+
+  div.green-splash span, div.red-splash span {
+    display: none;
+  }
+
+  @media screen and (max-width: 740px) {
+    .wrapper {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+
+    div.green-splash, div.red-splash {
+      width: 60vw;
+      height: 30vh;
+      margin: 2rem 0;
+    }
+
+  }
 
 </style>
